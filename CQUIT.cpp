e4f8dc8dbd8b48c7cf3b49a4b853f15fedbceb41
Alex-Zhai06 <alexander.davis2004@outlook.com>
2026-08-28 12:15:46 +0800

    可用版本1

---
+++ CQUIT.cpp
@@ -0,0 +1,55 @@
+﻿// CQUIT.cpp: 实现文件
+//
+
+#include "pch.h"
+#include "personal_zhaicheng.h"
+#include "afxdialogex.h"
+#include "CQUIT.h"
+
+
+// CQUIT 对话框
+
+IMPLEMENT_DYNAMIC(CQUIT, CDialogEx)
+
+CQUIT::CQUIT(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_CQUIT, pParent)
+{
+
+}
+
+CQUIT::~CQUIT()
+{
+}
+
+void CQUIT::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+
+BEGIN_MESSAGE_MAP(CQUIT, CDialogEx)
+	ON_BN_CLICKED(IDC_BUTTON1, &CQUIT::OnBnClickedButton1)
+	ON_BN_CLICKED(IDC_BUTTON2, &CQUIT::OnBnClickedButton2)
+END_MESSAGE_MAP()
+
+
+// CQUIT 消息处理程序
+
+void CQUIT::OnBnClickedButton1()
+{
+	// TODO: 在此添加控件通知处理程序代码
+	CWnd* pMainWnd = AfxGetApp()->m_pMainWnd;
+
+	if (pMainWnd != nullptr)
+	{
+		pMainWnd->DestroyWindow();
+	}
+
+	PostQuitMessage(0);
+}
+
+void CQUIT::OnBnClickedButton2()
+{
+	// TODO: 在此添加控件通知处理程序代码
+	OnCancel();
+}
---
+++ CQUIT.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+#include "afxdialogex.h"
+
+
+// CQUIT 对话框
+
+class CQUIT : public CDialogEx
+{
+	DECLARE_DYNAMIC(CQUIT)
+
+public:
+	CQUIT(CWnd* pParent = nullptr);   // 标准构造函数
+	virtual ~CQUIT();
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_CQUIT };
+#endif
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 支持
+
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedButton1();
+	afx_msg void OnBnClickedButton2();
+};
