e94122f38b06e175c701be0a70fbecce5aa60502
Alex-Zhai06 <alexander.davis2004@outlook.com>
2026-08-28 11:44:31 +0800

    Initial clean commit

---
+++ CWINwrong.cpp
@@ -0,0 +1,41 @@
+﻿// CWINwrong.cpp: 实现文件
+//
+
+#include "pch.h"
+#include "personal_zhaicheng.h"
+#include "afxdialogex.h"
+#include "CWINwrong.h"
+
+
+// CWINwrong 对话框
+
+IMPLEMENT_DYNAMIC(CWINwrong, CDialogEx)
+
+CWINwrong::CWINwrong(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_WR, pParent)
+{
+
+}
+
+CWINwrong::~CWINwrong()
+{
+}
+
+void CWINwrong::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+
+BEGIN_MESSAGE_MAP(CWINwrong, CDialogEx)
+	ON_BN_CLICKED(IDOK, &CWINwrong::OnBnClickedOk)
+END_MESSAGE_MAP()
+
+
+// CWINwrong 消息处理程序
+
+void CWINwrong::OnBnClickedOk()
+{
+	// TODO: 在此添加控件通知处理程序代码
+	CDialogEx::OnOK();
+}
---
+++ personal_zhaicheng.h
@@ -0,0 +1,32 @@
+﻿
+// personal_zhaicheng.h: PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含 'pch.h' 以生成 PCH"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CpersonalzhaichengApp:
+// 有关此类的实现，请参阅 personal_zhaicheng.cpp
+//
+
+class CpersonalzhaichengApp : public CWinApp
+{
+public:
+	CpersonalzhaichengApp();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CpersonalzhaichengApp theApp;
