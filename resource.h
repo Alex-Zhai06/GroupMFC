0fd92b5a912ca5c144bb663329375347eef38c2e
Alex-Zhai06 <alexander.davis2004@outlook.com>
2026-09-01 21:21:39 +0800

    完成球员页

--- resource.h
+++ resource.h
@@ -12,6 +12,9 @@
 #define IDB_BITMAP3                     133
 #define IDD_RESULT                      141
 #define IDB_BITMAP1                     143
+#define IDB_BITMAP_AGE400               145
+#define IDB_BITMAP_SPA400               146
+#define IDB_BITMAP_ENG400               147
 #define IDC_EDIT1                       1000
 #define IDC_EDIT2                       1001
 #define IDC_BUTTON1                     1002
@@ -21,15 +24,17 @@
 #define IDC_STATIC_CHOICE               1005
 #define IDC_BUTTON4                     1006
 #define IDC_PICTURE_BG                  1007
-#define IDC_BUTTON5                     1007
+#define IDC_PIC_AGE                     1008
+#define IDC_PIC_SPA                     1009
+#define IDC_PIC_ENG                     1010
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        144
+#define _APS_NEXT_RESOURCE_VALUE        148
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1008
+#define _APS_NEXT_CONTROL_VALUE         1011
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
